dd44e8737be61213abe3b79a99e62a7401488aa8
itsmealdo <aldosaputra.gooo@gmail.com>
2023-06-06 00:20:49 +0700

    create forum home, forum in post, and forum create post activity design

---
+++ app/src/main/java/com/team/temara/ui/forum/inpost/ForumPostActivity.kt
@@ -0,0 +1,18 @@
+package com.team.temara.ui.forum.inpost
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.team.temara.databinding.ForumActivityBinding
+import com.team.temara.databinding.ForumPostActivityBinding
+
+class ForumPostActivity : AppCompatActivity() {
+
+    private val binding: ForumPostActivityBinding by lazy {
+        ForumPostActivityBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
