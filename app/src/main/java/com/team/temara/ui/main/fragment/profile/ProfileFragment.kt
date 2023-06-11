a1eff4f49c2a6de6fee685cbfe28b2d6f5880de5
erwevkun <erwevkun@gmail.com>
2023-06-11 22:43:34 +0700

    update layout

--- app/src/main/java/com/team/temara/ui/main/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/team/temara/ui/main/fragment/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.team.temara.ui.main.fragment.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.team.temara.databinding.ProfileFragmentBinding
+import com.team.temara.ui.info.InfoUserActivity
+import com.team.temara.ui.profil.panicbutton.PanicButtonActivity
+import com.team.temara.ui.profil.password.PasswordActivity
+import com.team.temara.ui.profil.personaldata.PersonalDataActivity
 
 class ProfileFragment : Fragment() {
     private var _binding: ProfileFragmentBinding? = null
@@ -28,6 +33,15 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tvPersonalData.setOnClickListener {
+            startActivity(Intent(context, PersonalDataActivity::class.java))
+        }
+        binding.btnSos.setOnClickListener {
+            startActivity(Intent(context, PanicButtonActivity::class.java))
+        }
+        binding.tvUbahSandi.setOnClickListener{
+            startActivity(Intent(context, PasswordActivity::class.java))
+        }
         binding.btnLogout.setOnClickListener {
             profileFragmentViewModel.logout()
         }
